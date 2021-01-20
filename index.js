c480cfb3971708045a079d8e4e8133f83d12ada1
Michel Schmidt Torres <67986590+michelschmidttorres@users.noreply.github.com>
2021-01-20 12:55:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-shoppingList = [
-    { item: "pencil", amount: 1, price: 1.00 },         // 1,00
-    { item: "smartphone", amount: 15, price: 1599.99 }, // 23.999,85
-    { item: "notebook", amount: 7, price: 2399.80 },    // 16.798,60
-    { item: "mouse", amount: 37, price: 29.73 },        // 1.100,01
-    { item: "keyboard", amount: 37, price: 35.39 }      // 1.309,43
-]                                                       // TOTAL = 43.208,89
+function calculateTotalPerPerson(shoppingList, mailList) {
 
-mailList = [
-    "joao@abc.com",
-    "pedro@xpto.com.br",
-    "dev@stone.com.br"
-]
-
-function validateInputs(shoppingList, mailList) {
     if (!shoppingList)
         return 'shoppingList not defined'
     if (!mailList)
@@ -21,48 +8,44 @@ function validateInputs(shoppingList, mailList) {
         return "Empty shopping list"
     if (mailList.length === 0)
         return "Empty mail list"
-}
 
-function calculateTotalPerPerson(shoppingList, mailList) {
-
-    validateInputs(shoppingList, mailList)
-
-    // Calculate the total value
-    shoppingListTotal = shoppingList.reduce((total, currentItem) => {
-        return total += currentItem.amount * (currentItem.price * 100)
+     shoppingListTotal = shoppingList.reduce((total, currentItem) => {
+        return total += currentItem.amount * currentItem.price 
     }, 0)
 
-    // Index generated to control rounding of values
-    let index = mailList.length
-    let totalPerPerson = mailList.map(email => {
-        let value = shoppingListTotal / index
-        shoppingListTotal = shoppingListTotal - value
-        // The first email will always be rounded up
-        if (index == mailList.length) {
-            index--
-            return Math.ceil(value) / 100
-        } else {
-            index--
-            return Math.floor(value) / 100
-        }
-    })
+    let valuePerPerson =  shoppingListTotal / mailList.length 
 
-    // Map collection generated to return the desired list
     const billPerPerson = new Map()
-    for (let i = 0; i < mailList.length; i++) {
-        billPerPerson.set(mailList[i], totalPerPerson[i])
-    }
+
+    mailList.forEach((email, index) => {
+        if(mailList.length == index + 1) {
+            shoppingListTotal = shoppingListTotal - Math.floor(valuePerPerson)
+            billPerPerson.set(email, Math.floor(valuePerPerson + shoppingListTotal))
+        } else {
+            shoppingListTotal = shoppingListTotal - Math.floor(valuePerPerson) 
+            billPerPerson.set(email, Math.floor(valuePerPerson))
+        }
+    });
+
     return billPerPerson
 }
+
+shoppingList = [
+    { item: "pencil", amount: 1, price: 163 },         
+    { item: "smartphone", amount: 15, price: 159999 }, 
+    { item: "notebook", amount: 7, price: 239980 },    
+    { item: "mouse", amount: 37, price: 2973 },        
+    { item: "keyboard", amount: 37, price: 3539 }      
+]                                                       
+
+mailList = [
+    "joao@abc.com",
+    "pedro@xpto.com.br",
+    "dev@stone.com.br"
+]
 
 // Showing the result on the console when calling the function
 console.log(calculateTotalPerPerson(shoppingList, mailList))
 
-// Output
-// 'joao@abc.com' => 14402.97,
-// 'pedro@xpto.com.br' => 14402.96,
-// 'dev@stone.com.br' => 14402.96
 
-
-//      TOTAL    =      43208,89
 
