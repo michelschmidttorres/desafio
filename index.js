4440bb994aecb8dc098a14dd67c9180bc8660902
Michel Schmidt Torres <67986590+michelschmidttorres@users.noreply.github.com>
2021-01-23 12:46:36 -0300

    Update index.js
    Last version. Sended to Stone.

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 function calculateTotalPerPerson(shoppingList, mailList) {
 
-    if (!shoppingList)
-        return 'shoppingList not defined'
-    if (!mailList)
-        return 'mailList not defined'
     if (shoppingList.length === 0)
         return "Empty shopping list"
     if (mailList.length === 0)
         return "Empty mail list"
 
-    shoppingListTotal = shoppingList.reduce((total, currentItem) => {
-        return total += currentItem.amount * currentItem.price
+    const shoppingListTotal = shoppingList.reduce((total, currentItem) => {
+        return total += currentItem.amount * currentItem.price 
     }, 0)
 
-    const valuePerPerson = Math.floor(shoppingListTotal / mailList.length)
+    const valuePerPerson =  Math.floor(shoppingListTotal / mailList.length)
     let missingCents = shoppingListTotal % mailList.length
 
     return mailList.reduce((billPerPerson, email) => {
-        if (missingCents != 0) {
-            billPerPerson[email] = valuePerPerson + 1
-            missingCents--
+        if(missingCents > 0) {
+           billPerPerson[email] = valuePerPerson + 1
+           missingCents--
         } else {
-            billPerPerson[email] =  valuePerPerson
+            billPerPerson[email] = valuePerPerson
         }
         return billPerPerson
     }, {})
@@ -32,8 +28,8 @@ shoppingList = [
     { item: "smartphone", amount: 15, price: 159999 },
     { item: "notebook", amount: 7, price: 239980 },
     { item: "mouse", amount: 37, price: 2973 },
-    { item: "keyboard", amount: 37, price: 3539 }
-]
+    { item: "keyboard", amount: 37, price: 3539 }  
+]                                                       
 
 mailList = [
     "joao@abcd.com",
